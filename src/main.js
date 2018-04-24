8c0ef1945bc8f5a76715e3ae71ecf9e67a48f359
Guest User <Guest@Epicodus-9D.local>
2018-04-24 16:49:59 -0700

    ajax Practice

--- src/main.js
+++ src/main.js
@@ -1,2 +1,22 @@
 import $ from 'jquery';
 import 'bootstrap';
+
+$(document).ready(function() {
+  $('#theGif').click(function() {
+    setInterval(()=>{
+      $.ajax({
+        url: `https://api.giphy.com/v1/gifs/random?api_key=[API-KEY-GOES-HERE]&tag=goldblum&offset=2&rating=G&lang=en&height=400`,
+        type: 'GET',
+        data: {
+          format: 'json'
+        },
+        success: function(response) {
+          $('.errors').append(`<img src="${response.data.images.fixed_width.url}" alt="batman">`);
+        },
+        error: function() {
+          $('#errors').text("There was an error processing your request. Please try again.")
+        }
+      });
+    },1000/30);
+  });
+});
